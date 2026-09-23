f8579a7be521d7a0841d09465d3cf07b79f563de
EemaanH <ehafiz314090@gmail.com>
2026-09-23 15:28:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Project Planner loaded!");
+const addObjectButton = document.getElementById("addObject");
+const display = document.getElementById("display");
+
+addObjectButton.addEventListener("click", function()
+{
+const object = document.createElement("div");
+  object.classList.add("object");
+  display.appendChild(object);
+
+});
+  
