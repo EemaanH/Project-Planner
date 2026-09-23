ebaf7ddb7b64f3230990b1f5fc7a999bb4bf7d05
EemaanH <ehafiz314090@gmail.com>
2026-09-23 16:20:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const display = document.getElementById("display");
 
 addObjectButton.addEventListener("click", function()
 {
-const object = document.createElement("div");
-  object.classList.add("object");
-  display.appendChild(object);
+const width = Number(prompt("Enter object width in inches:"));
+const height - Number(prompt("Enter object height in inches"));
 
+  const object = document.createElemtn("div");
+
+  const scale = 25;
+
+  object.style.width = `${width * scale}px`;
+  object.style.height = `${height * scale}px`;
+
+  display.appendChild(object);
+  
 });
   
